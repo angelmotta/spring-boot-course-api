5bc5cc865b0c2c71a935d316ae5cb009a11a66fd
Angel Motta <angel.motta@utec.edu.pe>
2023-12-09 17:37:18 -0500

    setting up

---
+++ src/test/java/io/angelinux/CourseApi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.angelinux.CourseApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
