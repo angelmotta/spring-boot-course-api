38abc3196b7820fe046e70d7d59ccbd55628d627
Angel Motta <angel.motta@utec.edu.pe>
2023-12-09 18:01:18 -0500

    feat: topic class

--- src/main/java/io/angelinux/CourseApi/topic/TopicController.java
+++ src/main/java/io/angelinux/CourseApi/topic/TopicController.java
@@ -3,11 +3,18 @@ package io.angelinux.CourseApi.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
     @RequestMapping("/topics")
-    public String getAllTopics() {
-        return "All topics";
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring framework basics"),
+                new Topic("java", "Core java", "Core Java topics"),
+                new Topic("javascript", "Javascript essentials", "Javascript essentials topics")
+        );
     }
 }
