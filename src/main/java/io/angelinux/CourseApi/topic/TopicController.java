cd359f56b964de94dc19ea7bcadd2e359787a1b8
Angel Motta <angel.motta@utec.edu.pe>
2023-12-13 19:00:40 -0500

    feat: handling NotFound http status code

--- src/main/java/io/angelinux/CourseApi/topic/TopicController.java
+++ src/main/java/io/angelinux/CourseApi/topic/TopicController.java
@@ -1,6 +1,8 @@
 package io.angelinux.CourseApi.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,8 +19,12 @@ public class TopicController {
     }
 
     @RequestMapping("/topic/{id}")
-    public Optional<Topic> getTopic(@PathVariable String id) {
-        return topicService.getTopic(id);
+    public ResponseEntity<Topic> getTopic(@PathVariable String id) {
+        Optional<Topic> topicResponse = topicService.getTopic(id);
+        if (topicResponse.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(topicResponse.get());
     }
 
     @PostMapping(path = "/topics", consumes = "application/json")
